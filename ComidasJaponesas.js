58698438be4c0297338091d247a811fc70f808c0
NicoleCarolina <116591703+NicoleCarolina@users.noreply.github.com>
2023-03-17 12:38:15 -0300

    Add files via upload

---
+++ ComidasJaponesas.js
@@ -0,0 +1,21 @@
+
+
+let variable= 0;
+const arr [   =0
+
+    "sushi";
+    "tofu";
+    "woks";
+    "Yakimesyi";
+    "Yakisoba"
+
+    for(let i=0)
+
+    const comidas = {
+        one: {
+             sushi:"arroz";
+                   "alga" ;
+                   "pescado";
+    }
+        two { tofu}            }
+]
