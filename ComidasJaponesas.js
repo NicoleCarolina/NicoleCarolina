84c4106175f83c2c98b423579e1edff8cb05b7a2
NicoleCarolina <116591703+NicoleCarolina@users.noreply.github.com>
2023-03-28 01:47:54 -0300

    Add files via upload

---
+++ ComidasJaponesas.js
@@ -0,0 +1,40 @@
+
+
+const comidas = [   
+
+    "sushi";
+    "temaki";
+    "woks";
+    "Yakimeyi";
+
+  ]
+   
+    let arraycomidas = [  
+
+        {
+            sushi: "arroz",
+                   "alga",
+                   "pescado",
+    },
+        {   temaki:  "alga nori",
+                     "kanikama",
+                     "palta",
+                     "queso philadelphia",
+    },     
+        {  
+            woks : "arroz",
+                   "verduras",
+                   "langostino",
+            
+    },   
+          
+        {   yakimeyi : "con lomo",
+                       "con pollo",
+                       "con calamares y langostinos",
+    },    
+
+          ];
+
+          for(let i = 0 : i < arraycomidas.lenght; i++){
+            console.log("ingredientes: " + arraycomidas[i]);
+              }
